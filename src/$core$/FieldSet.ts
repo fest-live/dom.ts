df340beb41ad858d315b545669e7bd025ebc36c0
unit-404 <conway24b@gmail.com>
2024-10-31 15:06:44 +0700

    may works without wrapper

--- src/$core$/FieldSet.ts
+++ src/$core$/FieldSet.ts
@@ -1,49 +1,59 @@
 import { observeBySelector } from "./Observer";
 
 //
+const includeSelf = (target, selector)=>{
+    return (target.querySelector(selector) ?? (target.matches(selector) ? target : null));
+}
+
+//
 export const updateInput = (state, target)=>{
-    const input = target.querySelector("input:where([type=\"text\"], [type=\"number\"], [type=\"range\"])");
-    if (state && input) {
+    const selector = "input:where([type=\"text\"], [type=\"number\"], [type=\"range\"])";
+    const input    = includeSelf(target, selector);
+    const name     = input?.name || target?.dataset?.name || "";
+
+    //
+    if (state && input?.matches(selector)) {
         input.value = state[target?.dataset?.name];
         input.dispatchEvent(new Event("change", { bubbles: false, cancelable: true, }))
     }
 
-    // setup radio boxes
+    // setup radio boxes (requires wrapper)
     if (state) {
-        const radio = target.querySelector("input:where([type=\"radio\"][name=\""+target?.dataset?.name+"\"][value=\""+state[target?.dataset?.name]+"\"])");
+        const radio = includeSelf(target, "input:where([type=\"radio\"][name=\""+name+"\"][value=\""+state[name]+"\"])");
         if (state && radio && !radio?.checked) { radio?.click?.(); };
     }
 
     // setup check boxes
-    const checkbox = target.querySelector("input:where([type=\"checkbox\"])");
+    const checkbox = includeSelf(target, "input:where([type=\"checkbox\"])");
     if (state && checkbox) {
-        checkbox.checked = !!state[target?.dataset?.name];
+        checkbox.checked = !!state[name];
         checkbox.dispatchEvent(new Event("change", { bubbles: false, cancelable: true, }))
     }
 }
 
 //
-export const synchronizeInputs = (state, fields = document.documentElement, wrapper = ".u2-input")=>{
+export const synchronizeInputs = (state, wrapper = ".u2-input", fields = document.documentElement)=>{
 
     //
     const onChange = (ev)=>{
         const input  = ev.target;
         const target = input.closest(wrapper);
+        const name   = input.name || target?.dataset.name;
 
         //
         if (state) {
             if (input.matches("input:where([type=\"text\"], [type=\"number\"], [type=\"range\"])")) {
-                state[target.dataset.name] = (input.valueAsNumber != null && !isNaN(input.valueAsNumber)) ? input.valueAsNumber : input.value;
+                state[name] = (input.valueAsNumber != null && !isNaN(input.valueAsNumber)) ? input.valueAsNumber : input.value;
             }
 
             // any radio-box
             if (input?.matches("input[type=\"radio\"]:checked")) {
-                state[target.dataset.name] = input.value;
+                state[name] = input.value;
             }
 
             // any check-box
             if (input?.matches("input[type=\"checkbox\"]")) {
-                state[target.dataset.name] = input.checked;
+                state[name] = input.checked;
             }
         }
     };
